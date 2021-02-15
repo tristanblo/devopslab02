6a12204514afd48f3f2ff493eff5720a93106464
Tristan Blommers <grotle@upcmail.nl>
2021-02-15 22:17:47 +0100

    init

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
-# devopslab02
+# msc-se-devops-2021
+MSc Software Engineering (UvA) - DevOps and Cloud-based Software (2020-2021) | Repo for Lab assignments | Group "devops08" 
---
+++ tristan/lab02/python-flask-server/swagger_server/service/student_service.py
@@ -0,0 +1,67 @@
+import json
+import logging
+import os
+import tempfile
+
+from tinydb import TinyDB, Query
+from tinydb.middlewares import CachingMiddleware
+from functools import reduce
+import uuid
+
+from swagger_server.models import Student
+
+db_dir_path = tempfile.gettempdir()
+db_file_path = os.path.join(db_dir_path, "students.json")
+student_db = TinyDB(db_file_path)
+
+def add_student(student):
+    if not student.first_name:
+        return 'No first name found', 405
+    if not student.last_name:
+        return 'No last name found', 405
+
+    queries = []
+    query = Query()
+    queries.append(query.first_name == student.first_name)
+    queries.append(query.last_name == student.last_name)
+    queries.append(query.grades == student.grades)
+    query = reduce(lambda a, b: a & b, queries)
+    res = student_db.search(query)
+    if res:
+        return 'already exists', 409
+
+    doc_id = student_db.insert(student.to_dict())
+    student.student_id = doc_id
+    student_db.update({'student_id':doc_id}, query)
+    return student.student_id
+
+
+def get_student_by_id(student_id, subject):
+    student = student_db.get(doc_id=int(student_id))
+    if not student:
+        return student
+    student = Student.from_dict(student)
+    if not subject:
+        return student
+    if subject and subject in student.grades.keys():
+        return student
+
+def get_student_by_last_name(last_name):
+    queries = []
+    query = Query()
+    queries.append(query.last_name == str(last_name))
+    query = reduce(lambda a, b: a & b, queries)
+    res = student_db.search(query)
+    if not res:
+        return None
+    else:
+        return Student.from_dict(res[0])
+
+    return res
+
+def delete_student(student_id):
+    student = student_db.get(doc_id=int(student_id))
+    if not student:
+        return student
+    student_db.remove(doc_ids=[int(student_id)])
+    return student_id
